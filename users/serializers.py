afde79b5e42dbc613452e0e3bede86936a960207
unknown <gross3000@yandex.ru>
2026-03-13 10:22:10 +0300

    feat: add new app users and add migrate, urls,views

---
+++ users/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework.serializers import ModelSerializer
+
+from users.models import User
+
+
+class UserSerializer(ModelSerializer):
+    """Сериализатор по пользователям"""
+
+    class Meta:
+        model = User
+        fields = ( "id", "email", "first_name", "last_name", "country")
+
+
+
+class UserResetPassword(ModelSerializer):
+    """Сериалайзер для сброса пароля"""
+
+    class Meta:
+        model = User
+        fields = ("email",)
